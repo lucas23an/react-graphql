9177bcf01977740778f836843339011b5b6c3729
Lucas Braga <lucas23an@hotmail.com>
2017-10-25 02:58:41 -0200

    Files class 39

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class Songlist extends Component {
+    renderSongs() {
+        return this.props.data.songs.map(song => {
+            return (
+                <li>
+                    {song.title}
+                </li>
+            )
+        })
+    }
+
     render() {
+        if (this.props.data.loading) {
+            return <div>Loading...</div>;
+        }
+
         return (
             <div>
-                Songlist
+                {this.renderSongs()}
             </div>
         )
     }
 }
 
-export default Songlist;
+const query = gql`
+    {
+        songs {
+            title
+        }
+    }
+`;
+
+export default graphql(query) (Songlist);
